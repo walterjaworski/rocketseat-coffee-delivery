985a67cbc21a67f26d6a62b30e003759403533e1
Walter Jaworski <walter.jaworski@gmail.com>
2023-06-06 17:15:33 -0300

    add packages, configure eslint and default theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,32 @@
+export const defaultTheme = {
+  colors: {
+    backround: '#FAFAFA',
+    white: '#FFFFFF',
+    primary: {
+      light: '#F1E9C9',
+      main: '#DBAC2C',
+      dark: '#C47F17',
+    },
+    secondary: {
+      light: '#EBE5F9',
+      main: '#8047F8',
+      dark: '#4B2995',
+    },
+    base: {
+      title: '#272221',
+      subtitle: '#403937',
+      text: '#574F4D',
+      label: '#8D8686',
+      hover: '#D7D5D5',
+      button: '#E6E5E5',
+      input: '#EDEDED',
+      card: '#F3F2F2',
+    },
+  },
+  font: {
+    family: {
+      roboto: "'Roboto', sans-serif",
+      baloo: "'Baloo Chettan 2', cursive",
+    },
+  },
+}
