2b315bd684d50a033afbfbb3e315a0f46a36a2fa
Walter Jaworski <walter.jaworski@gmail.com>
2023-06-21 15:39:30 -0300

    new try

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,10 +6,29 @@ import { useContext, useEffect, useState } from 'react'
 
 import { CartContext, Product } from '../../contexts/CartContext'
 
+import photoAmerican from '../../assets/images/coffee/american.png'
+import photoArabic from '../../assets/images/coffee/arabic.png'
+import photoCapuccino from '../../assets/images/coffee/capuccino.png'
+import photoCreamyEspresso from '../../assets/images/coffee/creamy-espresso.png'
+import photoCuban from '../../assets/images/coffee/cuban.png'
+import photoEspresso from '../../assets/images/coffee/espresso.png'
+import photoHawaiian from '../../assets/images/coffee/hawaiian.png'
+import photoHotChocolate from '../../assets/images/coffee/hot-chocolate.png'
+import photoIceCoffee from '../../assets/images/coffee/ice-coffee.png'
+import photoIrish from '../../assets/images/coffee/irish.png'
+import photoLatte from '../../assets/images/coffee/latte.png'
+import photoMacchiato from '../../assets/images/coffee/macchiato.png'
+import photoMocaccino from '../../assets/images/coffee/mocaccino.png'
+import photoWithMilk from '../../assets/images/coffee/with-milk.png'
+
 interface CardProps {
   data: Product
   categories?: string[]
   variant?: 'large' | 'small'
+}
+
+interface PhotosProps {
+  [key: string]: string
 }
 
 export function CoffeeCard({ data, categories, variant = 'large' }: CardProps) {
@@ -23,6 +42,23 @@ export function CoffeeCard({ data, categories, variant = 'large' }: CardProps) {
     removeProduct,
     products,
   } = useContext(CartContext)
+
+  const photos: PhotosProps = {
+    'american': photoAmerican,
+    'arabic': photoArabic,
+    'capuccino': photoCapuccino,
+    'creamy-espresso': photoCreamyEspresso,
+    'cuban': photoCuban,
+    'espresso': photoEspresso,
+    'hawaiian': photoHawaiian,
+    'hot-chocolate': photoHotChocolate,
+    'ice-coffee': photoIceCoffee,
+    'irish': photoIrish,
+    'latte': photoLatte,
+    'macchiato': photoMacchiato,
+    'mocaccino': photoMocaccino,
+    'with-milk': photoWithMilk,
+  }
 
   const formatedPrice = data.price.toLocaleString('pt-BR', {
     minimumFractionDigits: 2,
@@ -73,7 +109,11 @@ export function CoffeeCard({ data, categories, variant = 'large' }: CardProps) {
   return (
     <S.Wrapper variant={variant}>
       <S.PhotoWrapper variant={variant}>
-        <img src={`${isRunningLocally() ? 'src' : ''}/assets/images/coffee/${data.image}.png`} alt="" />
+        {isRunningLocally() ? (
+          <img src={`src/assets/images/coffee/${data.image}.png`} alt="" />
+        ) : (
+          <img src={photos[data.image]} />
+        )}
       </S.PhotoWrapper>
       {largeCard && (
         <S.LabelWrapper>
